49a667f31c63e55a354e400363a755eea4521bdd
user.name : "alif33 <user.email : "alifhasan332@gmail.com>
2019-07-06 16:59:07 -0700

    @Post formats finished

--- functions.php
+++ functions.php
@@ -2,6 +2,6 @@
 add_action('after_setup_theme' , 'first_wp');
 
 function first_wp(){
-    add_theme_support('post-formats');
+    add_theme_support('post-formats', array( 'aside', 'gallery','audio','video'));
 } 
 ?>
