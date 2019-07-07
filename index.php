043f2b2a776a68b0e1e46bdd6e3fa5e3126e2572
user.name : "alif33 <user.email : "alifhasan332@gmail.com>
2019-07-06 23:14:18 -0700

    .

--- index.php
+++ index.php
@@ -27,7 +27,9 @@
           <?php } ?>
           <!-- Pager -->
           <div class="clearfix">
+
             <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+          
           </div>
         </div>
       </div>
