c32a884c5ed2f30e26578652e72d09af9bc35b15
user.name : "alif33 <user.email : "alifhasan332@gmail.com>
2019-07-06 20:06:54 -0700

    @Normal jquery/js enqueue

--- functions.php
+++ functions.php
@@ -12,6 +12,9 @@ function first_wp_enqueue(){
     wp_enqueue_style('custom', get_template_directory_uri().'/css/clean-blog.min.css');
     wp_enqueue_style('fonts-lora', '//fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic');
     wp_enqueue_style('fonts-open_sans', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800');
-    wp_enqueue_style( 'core', get_stylesheet_uri() );
+    wp_enqueue_style('core', get_stylesheet_uri());
+    //js script enqueue
+    wp_enqueue_script('bootsrap' , get_template_directory_uri().'/vendor/bootstrap/js/bootstrap.bundle.min.js' , array('jquery') , 2.0,true);
+    wp_enqueue_script('customs' , get_template_directory_uri().'/js/clean-blog.min.js' , array('jquery') , 1.0,true);
 }
 ?>
