309f485ed83538953612fc0240d64878c4a06d1e
user.name : "alif33 <user.email : "alifhasan332@gmail.com>
2019-07-06 21:50:14 -0700

    @Select part finished...

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ function first_wp(){
     add_theme_support('post-formats', array( 'aside', 'gallery','audio','video'));
     add_theme_support('post-thumbnails');
 } 
-add_action('wp_enqueue_scripts' , 'first_wp_enqueue');
+
 function first_wp_enqueue(){
     wp_enqueue_style('bootsrap', get_template_directory_uri().'/vendor/bootstrap/css/bootstrap.min.css');
     wp_enqueue_style('font-awesome', get_template_directory_uri().'/vendor/fontawesome-free/css/all.min.css');
@@ -16,5 +16,7 @@ function first_wp_enqueue(){
     //js script enqueue
     wp_enqueue_script('bootsrap' , get_template_directory_uri().'/vendor/bootstrap/js/bootstrap.bundle.min.js' , array('jquery') , 2.0,true);
     wp_enqueue_script('customs' , get_template_directory_uri().'/js/clean-blog.min.js' , array('jquery') , 1.0,true);
+//No necessary enqueue of jQuery.min.js
 }
+add_action('wp_enqueue_scripts' , 'first_wp_enqueue');
 ?>
