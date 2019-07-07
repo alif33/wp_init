c4a99ac8c6ee1a51e0aceffa34fb78c64b0ff73f
user.name : "alif33 <user.email : "alifhasan332@gmail.com>
2019-07-06 19:37:06 -0700

    @standard way enqueue css(download)

--- functions.php
+++ functions.php
@@ -5,4 +5,9 @@ function first_wp(){
     add_theme_support('post-formats', array( 'aside', 'gallery','audio','video'));
     add_theme_support('post-thumbnails');
 } 
+add_action('wp_enqueue_scripts' , 'first_wp_enqueue');
+function first_wp_enqueue(){
+    wp_enqueue_style('bootsrap', get_template_directory_uri().'/vendor/bootstrap/css/bootstrap.min.css');
+    wp_enqueue_style('custom', get_template_directory_uri().'/css/clean-blog.min.css');
+}
 ?>
--- header.php
+++ header.php
@@ -11,7 +11,6 @@
     <title>code4webs.com - Bootstrap Theme</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="<?php echo get_template_directory_uri();?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
     <link href="<?php echo get_template_directory_uri().'/vendor/fontawesome-free/css/all.min.css'?>" rel="stylesheet" type="text/css">
@@ -19,7 +18,7 @@
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
     <!-- Custom styles for this template -->
-    <link href="<?php echo get_template_directory_uri().'/css/clean-blog.min.css'?>" rel="stylesheet">
+ 
 <?php wp_head();?>
 </head>
 
