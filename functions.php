a70538cb4c33acf58eed47d6bc1dcbb428502fce
user.name : "alif33 <user.email : "alifhasan332@gmail.com>
2019-07-06 19:41:44 -0700

    .

--- functions.php
+++ functions.php
@@ -8,6 +8,7 @@ function first_wp(){
 add_action('wp_enqueue_scripts' , 'first_wp_enqueue');
 function first_wp_enqueue(){
     wp_enqueue_style('bootsrap', get_template_directory_uri().'/vendor/bootstrap/css/bootstrap.min.css');
+    wp_enqueue_style('font-awesome', get_template_directory_uri().'/vendor/fontawesome-free/css/all.min.css');
     wp_enqueue_style('custom', get_template_directory_uri().'/css/clean-blog.min.css');
 }
 ?>
