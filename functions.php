cb49917275a6abf016cf4f55b9c6cab3a58a0e73
user.name : "alif33 <user.email : "alifhasan332@gmail.com>
2019-07-06 17:03:11 -0700

    @post thumbnails for featured iamge

--- functions.php
+++ functions.php
@@ -3,5 +3,6 @@ add_action('after_setup_theme' , 'first_wp');
 
 function first_wp(){
     add_theme_support('post-formats', array( 'aside', 'gallery','audio','video'));
+    add_theme_support('post-thumbnails');
 } 
 ?>
